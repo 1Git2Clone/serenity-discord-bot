269e9dc002b62350b0a685254e20adbcd8a94317
1Kill2Steal <imliterallyhim@duck.com>
2024-02-28 14:41:10 +0200

    Fixed commenting convention

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
 // #region Notes about the external imports
-/*
- * The tokio async runtime:
- * - https://tokio.rs/
- * - https://github.com/tokio-rs/tokio/
- * NOTE: It's kind of necessary to use it for better responsiveness, especially in bigger servers.
- *
- * The serenity-rs API for the discord bot functionality:
- * - https://github.com/serenity-rs/serenity/
- * NOTE: It has very nice and comprehensive examples under this folder:
- * - https://github.com/serenity-rs/serenity/tree/current/examples
- *
- * lazy_static:
- * - https://github.com/rust-lang-nursery/lazy-static.rs
- * Why? Well its for the ease of modularity.
- * Having the data seperated in a different folder
- * makes the project more organized (at least in my opinion).
- *
- */
+//
+// The tokio async runtime:
+// - https://tokio.rs/
+// - https://github.com/tokio-rs/tokio/
+// NOTE: It's kind of necessary to use it for better responsiveness, especially in bigger servers.
+//
+// The serenity-rs API for the discord bot functionality:
+// - https://github.com/serenity-rs/serenity/
+// NOTE: It has very nice and comprehensive examples under this folder:
+// - https://github.com/serenity-rs/serenity/tree/current/examples
+//
+// lazy_static:
+// - https://github.com/rust-lang-nursery/lazy-static.rs
+// Why? Well its for the ease of modularity.
+// Having the data seperated in a different folder
+// makes the project more organized (at least in my opinion).
+//
+//
 
 // #endregion
 
 // #region All imports (./lib.rs)
 
-/*
- * The bot_data like the BOT_TOKEN is handled using the dotenv-rs dependency!
- * - https://github.com/dotenv-rs/dotenv/
- *
- */
+//
+// The bot_data like the BOT_TOKEN is handled using the dotenv-rs dependency!
+// - https://github.com/dotenv-rs/dotenv/
+//
+//
 
 mod commands;
 use commands::general_commands::age;
