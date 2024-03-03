7d0e59516cb413f82ec8415c6efee261628837ab
1Kill2Steal <imliterallyhim@duck.com>
2024-03-04 00:30:47 +0200

    Updated some comments

--- src/commands/general_commands.rs
+++ src/commands/general_commands.rs
@@ -14,7 +14,9 @@ pub async fn help(
         command.as_deref(),
         poise::builtins::HelpConfiguration {
             extra_text_at_bottom:
-                "* Note: You can use\n/help command_name\nfor more details on a specific command.",
+                // OLD.
+                // "* Note: You can use\n/help command_name\nfor more details on a specific command.",
+                "* Note: You can use \'/help <command_name>\' for more details on a specific command.",
             ..Default::default()
         },
     )
@@ -25,7 +27,9 @@ pub async fn help(
     }
 
     ctx.defer_ephemeral().await?;
-    let reply_text = "This bot has been made using Rust with the [serenity-rs](<https://github.com/serenity-rs/serenity>) and [poise](<https://github.com/serenity-rs/poise>) frameworks.\nIt's open source and hosted on my [github profile](<https://github.com/1Kill2Steal/serenity-discord-bot>).\nUnfortunately you can't select users by replying to messages yet. I'm just not sure at how to implement it. *(Skill Issue...)*";
+    // OLD.
+    // let reply_text = "This bot has been made using Rust with the [serenity-rs](<https://github.com/serenity-rs/serenity>) and [poise](<https://github.com/serenity-rs/poise>) frameworks.\nIt's open source and hosted on my [github profile](<https://github.com/1Kill2Steal/serenity-discord-bot>).\nUnfortunately you can't select users by replying to messages yet. I'm just not sure at how to implement it. *(Skill Issue...)*";
+    let reply_text = "This bot has been made using Rust with the [serenity-rs](<https://github.com/serenity-rs/serenity>) and [poise](<https://github.com/serenity-rs/poise>) frameworks.\nIt's open source and hosted on my [github profile](<https://github.com/1Kill2Steal/serenity-discord-bot>).\n";
     let reply = poise::CreateReply::default()
         .content(reply_text)
         .ephemeral(true);
