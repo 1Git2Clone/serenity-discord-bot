80c83b7a46cfcf79d221a9ec7f2876a5a801f6dc
1Kill2Steal <ivankirilov640@gmail.com>
2024-03-31 10:06:15 +0300

    Removed the /boom command from the util_commands.

--- src/commands/util_commands.rs
+++ src/commands/util_commands.rs
@@ -33,22 +33,3 @@ pub async fn uptime(ctx: Context<'_>) -> Result<(), Error> {
 
     Ok(())
 }
-
-/// Just try it.
-#[poise::command(slash_command, prefix_command, rename = "uptime")]
-pub async fn boom(ctx: Context<'_>) -> Result<(), Error> {
-    let bot_user = ctx.http().get_current_user().await?;
-    let bot_avatar = bot_user.face().replace(".webp", ".png");
-
-    ctx.send(
-        poise::CreateReply::default().embed(
-            serenity::CreateEmbed::default()
-                .image("https://cdn.discordapp.com/attachments/1129364410566193192/1223359511356641321/ea022b6f5e25129f8c865b6b2d8e2f33.jpg?ex=66199154&is=66071c54&hm=3fc8357942f1ea01c76b2c249c6db654ef6572d00a5e7d65af4de3266d39ae6b&")
-                .color((255, 0, 0))
-                .footer(serenity::CreateEmbedFooter::new(bot_user.tag()).icon_url(bot_avatar)),
-        ),
-    )
-    .await?;
-
-    Ok(())
-}
