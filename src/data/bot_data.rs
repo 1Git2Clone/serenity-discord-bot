c23b47d55ad547eb539f21a0ff4f71e4a45586bb
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-10-19 17:38:40 +0300

    Small style changes

--- src/data/bot_data.rs
+++ src/data/bot_data.rs
@@ -1,22 +1,27 @@
-use crate::enums::schemas::{DatabaseSchema, DatabaseSchema::*};
+use crate::enums::schemas::DatabaseSchema;
 use std::collections::HashMap;
 
 use lazy_static::lazy_static;
+
+pub const DATABASE_FILENAME: &str = "database/bot_database.sqlite";
+pub const DATABASE_USERS: &str = "user_stats";
 
 lazy_static! {
     #[derive(Debug)] // So it can be printed in main.rs (you shouldn't do it tho)
     pub(crate) static ref BOT_TOKEN: String =
         std::env::var("BOT_TOKEN").expect("Expected a token in the dotenv file.");
     pub(crate) static ref START_TIME: std::time::Instant = std::time::Instant::now();
-    pub(crate) static ref DATABASE_FILENAME: &'static str = "database/bot_database.sqlite";
     #[derive(Debug)]
-    pub(crate) static ref DATABASE_USERS: &'static str = "user_stats";
-    pub(crate) static ref DATABASE_COLUMNS: HashMap<DatabaseSchema, &'static str> = HashMap::from([
-        (UserId, "user_id"),
-        (GuildId, "guild_id"),
-        (ExperiencePoints, "experience_points"),
-        (Level, "level"),
-        (LastQueryTimestamp, "last_query_timestamp")
-    ]);
+    pub(crate) static ref DATABASE_COLUMNS: HashMap<DatabaseSchema, &'static str> = {
+        use crate::enums::schemas::DatabaseSchema as DbSch;
+
+        HashMap::from([
+            (DbSch::UserId, "user_id"),
+            (DbSch::GuildId, "guild_id"),
+            (DbSch::ExperiencePoints, "experience_points"),
+            (DbSch::Level, "level"),
+            (DbSch::LastQueryTimestamp, "last_query_timestamp")
+        ])
+    };
     pub(crate) static ref XP_COOLDOWN_NUMBER_SECS: i64 = 60;
 }
