9c061705ae351a2554cf17fd24fdd2743f5a49a3
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-12-15 23:27:07 +0200

    Reformat

--- src/enums/emojis.rs
+++ src/enums/emojis.rs
@@ -19,8 +19,14 @@
 ///     }
 /// }
 ///
-/// assert_eq!(Emojis::EmojiOne.to_string(), "<:EmojiOne:123456789>".to_string());
-/// assert_eq!(Emojis::EmojiTwo.to_string(), "<:EmojiTwo:987654321>".to_string());
+/// assert_eq!(
+///     Emojis::EmojiOne.to_string(),
+///     "<:EmojiOne:123456789>".to_string()
+/// );
+/// assert_eq!(
+///     Emojis::EmojiTwo.to_string(),
+///     "<:EmojiTwo:987654321>".to_string()
+/// );
 /// ```
 #[macro_export]
 macro_rules! display_emoji_impl {
@@ -28,20 +34,14 @@ macro_rules! display_emoji_impl {
         impl std::fmt::Display for $enum_name {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 match self {
-                    $(
-                        Self::$variant => {
-                            write!(
-                                f,
-                                concat!(
-                                    "<:",
-                                    stringify!($variant),
-                                    ":",
-                                    $id,
-                                    ">"
-                                )
+                    $(Self::$variant => {
+                        write!(
+                            f,
+                            concat!(
+                                "<:", stringify!($variant), ":", $id, ">"
                             )
-                        }
-                    )*
+                        )
+                    })*
                 }
             }
         }
