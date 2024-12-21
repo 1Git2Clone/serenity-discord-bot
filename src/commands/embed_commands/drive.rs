aca452fb9b975b40e9db8ec98de757c2b79467fa
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-12-21 12:57:09 +0200

    Add footer

--- src/commands/embed_commands/drive.rs
+++ src/commands/embed_commands/drive.rs
@@ -4,10 +4,15 @@ use super::*;
 #[poise::command(slash_command, prefix_command)]
 pub async fn drive(ctx: Context<'_>) -> Result<(), Error> {
     let embed_item: &str = cmd_utils::get_rand_embed_from_type(&EmbedType::RyanGoslingDrive)?;
+    let bot_user = Arc::clone(&ctx.data().bot_user);
 
     let embed = serenity::CreateEmbed::new()
         .color((255, 0, 0))
-        .image(embed_item);
+        .image(embed_item)
+        .footer(
+            serenity::CreateEmbedFooter::new(bot_user.tag())
+                .icon_url(Arc::clone(&ctx.data().bot_avatar).to_string()),
+        );
     let full_respone = poise::CreateReply::default().embed(embed);
     ctx.send(full_respone).await?;
 
