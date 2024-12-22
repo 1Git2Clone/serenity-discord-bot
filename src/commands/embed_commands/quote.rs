cd425b9e9a175eb8067b035adfb0ad3549dd9c1a
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-12-22 23:16:09 +0200

    Update the quote command description

--- src/commands/embed_commands/quote.rs
+++ src/commands/embed_commands/quote.rs
@@ -4,7 +4,7 @@ use crate::data::bot_data::HU_TAO_VOICELINES_JP;
 
 use super::*;
 
-/// Send a peek GIF in the chat (you lurker)
+/// Send a Hu Tao quote!
 #[poise::command(prefix_command, slash_command)]
 pub async fn quote(ctx: Context<'_>) -> Result<(), Error> {
     let response = HU_TAO_VOICELINES_JP
