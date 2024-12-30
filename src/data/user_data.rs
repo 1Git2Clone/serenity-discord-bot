1ad810a83433cd296f765785a596d2c717a29a78
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-12-30 19:33:54 +0200

    Remove unnecessary `Arc<T>` wrapper around `USER_COOLDOWNS`

--- src/data/user_data.rs
+++ src/data/user_data.rs
@@ -2,8 +2,8 @@ use crate::prelude::*;
 use std::sync::Mutex;
 
 pub type UserData = HashMap<(UserId, GuildId), i64>;
-pub type UserCooldowns = Arc<Mutex<UserData>>;
+pub type UserCooldowns = Mutex<UserData>;
 
 lazy_static! {
-    pub static ref USER_COOLDOWNS: UserCooldowns = Arc::new(Mutex::new(HashMap::new()));
+    pub static ref USER_COOLDOWNS: UserCooldowns = Mutex::new(HashMap::new());
 }
