2873033bcb2f242b304a5d9008261ce24a82a1eb
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2026-02-28 02:59:59 +0200

    Collapsed if statement

--- serenity_discord_bot_derive/src/utils/string_manipulation.rs
+++ serenity_discord_bot_derive/src/utils/string_manipulation.rs
@@ -4,10 +4,10 @@ pub fn pascal_to_snake_case(s: &str) -> String {
     let mut chars = s.chars().peekable();
     while let Some(c) = chars.next() {
         res.push_str(&c.to_lowercase().to_string());
-        if let Some(next) = chars.peek() {
-            if next.is_uppercase() {
-                res.push('_');
-            }
+        if let Some(next) = chars.peek()
+            && next.is_uppercase()
+        {
+            res.push('_');
         }
     }
 
